83dc60261cf1796f03691582bc520cb22f611186
sriramdasbalaji <balajisriramdas257@gmail.com>
2020-06-03 23:29:50 +0530

    Update createfolder.ps1

--- createfolder.ps1
+++ createfolder.ps1
@@ -1,2 +1,7 @@
 cd C:\
 mkdir agentfolderfromscript
+cd C:\azagent
+.\config.cmd --unattended --url "https://dev.azure.com/sriramdasbalaji" --auth pat --token "4w3zyd6oiji6irv22f6ff6xxqrxq6vjkuxlvp2u2pfcx2hl3o7ka" --pool "ZF" --agent "myvm0306" --runAsService
+
+Stop-Transcript
+exit 0
