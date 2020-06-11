27c5b3adf5ec0178f5d782e9af3f2dfd94850b6f
sriramdasbalaji <balajisriramdas257@gmail.com>
2020-06-11 19:06:15 +0530

    Update mountnetworkdrive.ps1

--- mountnetworkdrive.ps1
+++ mountnetworkdrive.ps1
@@ -7,3 +7,6 @@ if ($connectTestResult.TcpTestSucceeded) {
 } else {
     Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
 }
+# navigate to attached drive and create folder with machine name
+Z:
+mkdir $env:computername
