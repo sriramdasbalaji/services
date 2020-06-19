761eb2b6c5a6ce3bf57b286a1462f193c3db821a
sriramdasbalaji <balajisriramdas257@gmail.com>
2020-06-19 19:57:08 +0530

    Create linuxagent.sh

---
+++ linuxagent.sh
@@ -0,0 +1,8 @@
+print_header "Configuring Azure Pipelines agent..."
+mkdir myagent && cd myagent
+wget https://vstsagentpackage.azureedge.net/agent/2.170.1/vsts-agent-linux-x64-2.170.1.tar.gz
+tar zxvf vsts-agent-linux-x64-2.170.1.tar.gz
+./config.sh --unattended --url https://dev.azure.com/sriramdasbalaji --auth pat --token "4w3zyd6oiji6irv22f6ff6xxqrxq6vjkuxlvp2u2pfcx2hl3o7ka" --acceptTeeEula --pool "ZF" --agent $HOSTNAME --replace & wait $!
+
+# AgentService.js understands how to handle agent self-update and restart
+exec ./externals/node/bin/node ./bin/AgentService.js interactive
