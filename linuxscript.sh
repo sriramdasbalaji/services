68e712a94e2f05b9cf00f04397d91efe0f6eff9c
sriramdasbalaji <balajisriramdas257@gmail.com>
2020-06-19 22:27:09 +0530

    Update linuxscript.sh

--- linuxscript.sh
+++ linuxscript.sh
@@ -1,3 +1,8 @@
 cd /home/vmadmin
 mkdir myagent && cd myagent
 wget https://vstsagentpackage.azureedge.net/agent/2.170.1/vsts-agent-linux-x64-2.170.1.tar.gz
+tar zxvf vsts-agent-linux-x64-2.170.1.tar.gz
+./config.sh --unattended --url https://dev.azure.com/sriramdasbalaji --auth pat --token "4w3zyd6oiji6irv22f6ff6xxqrxq6vjkuxlvp2u2pfcx2hl3o7ka" --acceptTeeEula --pool "ZF" --agent $HOSTNAME --replace & wait $!
+
+# AgentService.js understands how to handle agent self-update and restart
+exec ./externals/node/bin/node ./bin/AgentService.js interactive
