4369f724539d91aa0cfee4b0c39df50ac44b426f
sriramdasbalaji <balajisriramdas257@gmail.com>
2020-06-21 21:59:47 +0530

    Update ubuntuvm.sh

--- ubuntuvm.sh
+++ ubuntuvm.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-cd /home/azureuser;mkdir azagent;cd azagent;curl -fkSL -o vstsagent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.170.1/vsts-agent-linux-x64-2.170.1.tar.gz;sleep 5;tar -zxvf vstsagent.tar.gz;sleep 10;./config.sh --auth pat --token 4w3zyd6oiji6irv22f6ff6xxqrxq6vjkuxlvp2u2pfcx2hl3o7ka --acceptTeeEula --pool ZF  --agent ubuntu18042 --url https://dev.azure.com/sriramdasbalaji/ --work _work --replace --runasservice;sudo ./svc.sh install;sudo ./svc.sh start;
+cd /home/azureuser;mkdir azagent;cd azagent;curl -fkSL -o vstsagent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.170.1/vsts-agent-linux-x64-2.170.1.tar.gz;sleep 5;tar -zxvf vstsagent.tar.gz;sleep 10;
+sudo -E bin/Agent.Listener configure --unattended --nostart --replace --acceptteeeula --pool ZF  --agent ubuntu18973  --url https://dev.azure.com/sriramdasbalaji/ --auth pat --token 4w3zyd6oiji6irv22f6ff6xxqrxq6vjkuxlvp2u2pfcx2hl3o7ka
