399453615570dd5221d6af3ac11f8311a34fe0c8
sriramdasbalaji <balajisriramdas257@gmail.com>
2020-06-29 14:54:25 +0530

    Update windowsazagent.ps1

--- windowsazagent.ps1
+++ windowsazagent.ps1
@@ -1,1 +1,9 @@
+cd C:\
+mkdir azagent
+cd C:\azagent
+Invoke-WebRequest -Uri https://vstsagentpackage.azureedge.net/agent/2.170.1/vsts-agent-win-x64-2.170.1.zip -OutFile c:\azagent\azagent.zip
+Add-Type -AssemblyName System.IO.Compression.FileSystem;[System.IO.Compression.ZipFile]::ExtractToDirectory( "c:\azagent\azagent.zip", "$PWD");
+.\config.cmd --unattended --url "https://dev.azure.com/sriramdasbalaji" --auth pat --token "4w3zyd6oiji6irv22f6ff6xxqrxq6vjkuxlvp2u2pfcx2hl3o7ka" --pool "ZF" --agent "myvm0306" --runAsService
 
+Stop-Transcript
+exit 0
